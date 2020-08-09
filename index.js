e579054dd0a365b6fcaf371072ba3f2046a96567
Ron <ronmada@s.braude.com>
2020-08-09 18:47:39 +0300

    inital 5th commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('<h1>hello world</h1>');
+    res.send(` hello port is : ${PORT}`);
 });
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-console.log(` PORT IS: ${port}`);
+console.log(` PORT IS: ${PORT}`);
