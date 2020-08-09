6807f67793fac0e07401c1c856b1e2ccf544bf6c
Ron <ronmada@s.braude.com>
2020-08-09 18:42:05 +0300

    inital 3rd commit

--- index.js
+++ index.js
@@ -10,3 +10,4 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log(` PORT IS: ${port}`)
