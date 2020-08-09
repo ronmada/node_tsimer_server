cad79b1ffc3ae709901fa726fa80e46c28fd7e9c
Ron <ronmada@s.braude.com>
2020-08-09 18:40:34 +0300

    inital 2nd commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req,res) => {
     res.send('<h1>hello world</h1>');
 });
 
-
+app.get('/favicon.ico', (req, res) => res.status(204));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
