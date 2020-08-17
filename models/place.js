f66db7813d74e874e74e83a67c68ada72c14b9cc
Ron <ronmada@s.braude.com>
2020-08-17 22:55:38 +0300

    commiter

---
+++ models/place.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+    name: String,
+    kind_of_place: String,
+    area: String,
+    description: String,
+    score_avg: String,
+    wifi: Boolean,
+    handicap: Boolean,
+    swimming_pool: Boolean,
+    animal: Boolean,
+    shaabaat_keep: Boolean,
+    price_night: Number,
+});
+
+// const Place = mongoose.model("Place", placeSchema);
+
+// const place = new Place({
+//     name: 'hello',
+//     price_night: 25,
+//     wifi: true
+// });
+
+// place.save();
+module.exports = mongoose.model("Place", placeSchema);
